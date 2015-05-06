2beeb3aa18964f0982d1e12194b718c6c93b0cde
Stephen Turley <turley714@gmail.com>
2015-05-06 00:16:17 -0400

    Fixed file names to follow convention and added a GroceryList class

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # GroceryTotes
 [![Build Status](https://drone.io/github.com/StephenTurley/GroceryTotes/status.png)](https://drone.io/github.com/StephenTurley/GroceryTotes/latest)
+
 Shopping list thingy
---
+++ test/lib/grocery_list_test.dart
@@ -0,0 +1,12 @@
+import 'package:grocery_totes/grocery_list.dart';
+import 'package:test/test.dart';
+
+main(){
+
+  test('Should be able to contruct a GroceryList', () {
+    var underTest = new GroceryList();
+
+    expect(underTest, new isInstanceOf<GroceryList>() );
+  });
+
+}
