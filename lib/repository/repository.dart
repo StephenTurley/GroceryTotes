c227a62ea3736d584c3d3b67537475bcfde7ffcc
Stephen Turley <turley714@gmail.com>
2015-05-06 22:10:51 -0400

    Forogot to add new repository lib

---
+++ lib/repository/repository.dart
@@ -0,0 +1,12 @@
+library repository;
+
+import 'package:grocery_totes/domain/grocery_list.dart';
+
+class GroceryListRepository implements Repository {
+  GroceryList getGroceryList() => throw new UnimplementedError();
+}
+
+class Repository {
+  Repository(){}
+}
+
