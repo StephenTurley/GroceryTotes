b0457b671be77d248731c5970327291876d9ecfb
Stephen Turley <turley714@gmail.com>
2015-06-15 00:09:36 -0400

    added an api endpoint and some integration tests

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var path = require('path');
-var app = express();
+
+var app = exports.app = express();
+
 
 var index = require('./routes/index');
+var api = require('./routes/api/api');
 
 app.use(express.static(path.join(__dirname,'client/dist/')));
 
 app.get('/', index.get);
+app.get('/api/list', api.list.getList);
 
-var port = process.env.PORT || 8080;
+function start(){
+    var port = process.env.PORT || 8080;
+    
+    app.listen(port, function(){
+        console.log('grocery_totes is running on port: ' + port);
+    });
+};
 
-app.listen(port, function(){
-    console.log('grocery_totes is running on port: ' + port);
-});
+exports.start = start;
+
+
---
+++ spec/integration-spec.js
@@ -0,0 +1,40 @@
+var request = require('supertest');
+
+var app = require('../app').app;
+
+
+describe('GET /', function(){
+	
+	it(' should repsond with 200', function(done){
+		request(app)
+		.get('/')
+		.expect(200)
+		.end(function(err, res){
+			detectError(err, done);
+		});
+	});
+	
+});
+
+describe('GET api/', function(){
+	describe('list', function(){
+		it('should respond with json', function(done){
+			request(app)
+			.get('/api/list')
+			.set('Accept', 'application/json')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end(function(err, res){
+				detectError(err, done);
+			});
+		});
+	});
+});
+
+function detectError(err, done) {
+	if(err){
+		done.fail(err);
+	} else {
+		done();
+	}
+}
