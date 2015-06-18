35edd9c17ba87868cdf94a8eeb11b002ad743838
Stephen Turley <turley714@gmail.com>
2015-06-18 00:10:55 -0400

    Added e2e test

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var forever =  require('forever');
 
 
 var srcs = {
-	test : 'spec/**/*-spec.js'
+	test : 'spec/**/*-spec.js',
+	e2e : 'e2e/**/*-spec.js'
 };
 
 gulp.task('test', function(){
 	return gulp.src(srcs.test)
 			.pipe($.jasmine());
+});
+
+gulp.task('e2e', function(){
+	return gulp.src(srcs.e2e)
+		.pipe($.jasmine())
+		.pipe($.debug());
 });
